b9ec45793d2075e4a5eb1d15d8f6e36588680eb4
yann-catlinn <y.catrilelbun@alumnos.duoc.cl>
2020-08-02 09:05:32 -0400

    I've added content to index, linked Bootstrap's  CDN, linked CSS stylesheet with styles for landing page, linked my own script from external JS file, linked fonts Google and font Awesome icons, added assets directory for: images, CSS iwth stylesheet, JS with script for smooth scroll, projects that iv'e done before.

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+$(document).on('click', 'a[href^="#"]', function(e) {
+    e.preventDefault();
+    $('html, body').stop().animate({
+        scrollTop: $($(this).attr('href')).offset().top
+    }, 500, 'linear');
+});
